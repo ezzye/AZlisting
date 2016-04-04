758b7f5dc8b98f1134acd6798f2e220c7968c28f
Ezzy Elliott <ezzy.elliott@gmail.com>
2016-04-04 18:35:33 +0100

    Pass inital feature test by setting up page look and feel

--- app/app.js
+++ app/app.js
@@ -1,5 +1,65 @@
+'use strict';
+
 var azlist = angular.module('azlist',['ui.bootstrap']);
 
-azlist.controller('azlistCtrl',function($scope) {
-  $scope.hello = "Hello world";
+azlist.controller('azlistCtrl',function($scope,getazlist) {
+  $scope.items = getazlist.filteredlist;
+  $scope.currentPage = 1;
+  $scope.numberOfItems = 44;
+  $scope.maxSize = 5;
+
+  $scope.radioModel="A";
+
+
+});
+
+azlist.factory('getazlist',function() {
+  var factory = {};
+  factory.filteredlist = [
+                            {
+                              id: "b07610mb",
+                              live: false,
+                              type: "episode",
+                              title: "Super League Show",
+                              images: {
+                              type: "image",
+                              standard: "http://ichef.bbci.co.uk/images/ic/96x96/p03j15jb.jpg",
+                              inherited_from: "brand"
+                              }
+                            },
+                            {
+                              id: "b07610mb",
+                              live: false,
+                              type: "episode",
+                              title: "Next League Show",
+                              images: {
+                              type: "image",
+                              standard: "http://ichef.bbci.co.uk/images/ic/96x96/p03j15jb.jpg",
+                              inherited_from: "brand"
+                              }
+                            },
+                            {
+                              id: "b07610mb",
+                              live: false,
+                              type: "episode",
+                              title: "Another League Show",
+                              images: {
+                              type: "image",
+                              standard: "http://ichef.bbci.co.uk/images/ic/96x96/p03j15jb.jpg",
+                              inherited_from: "brand"
+                              }
+                            },
+                            {
+                              id: "b07610mb",
+                              live: false,
+                              type: "episode",
+                              title: "Onemore League Show",
+                              images: {
+                              type: "image",
+                              standard: "http://ichef.bbci.co.uk/images/ic/96x96/p03j15jb.jpg",
+                              inherited_from: "brand"
+                              },
+                            }
+                          ];
+  return factory;
 });
--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -1,15 +1,27 @@
 'use strict';
 
+
 describe('listing page',function() {
 
 
-//loads page for letter A page 1
+  //initally loads page for letter A page 1
 
 
-//listing first page of a letter - using API for letter endpoint
+
+  it('load page 1 for A initally',function() {
+    browser.get('http://localhost:8000/app');
+    var pageLetter=element(by.id('StartingLetter'));
+    var pageNumber=element(by.id('PageNumber'));
+    expect(pageLetter.getText()).toEqual('Programmes Starting with: A');
+    expect(pageNumber.getText()).toEqual('Page: 1');
+    expect(browser.getTitle()).toEqual('A-Z Listing Web Page');
+
+  });
+
+  //listing first page of a letter - using API for letter endpoint
 
 
-//listing for all pages of a letter - using API for page number
+  //listing for all pages of a letter - using API for page number
 
 
 
