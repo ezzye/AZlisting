8cd344b335df911bec56d1a536385b459609ba11
Ezzy Elliott <ezzy.elliott@gmail.com>
2016-04-05 22:38:20 +0100

    set port to 8888

--- server.js
+++ server.js
@@ -5,12 +5,16 @@ var express = require('express');
 
 var app = express();
 
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8888;
+
 var azprocessor = require('./azprocessor');
 
 app.get(/programme/, azprocessor.getmyAPI, azprocessor.getexternalAPI, azprocessor.myazlisting);
 
 app.use('/', express.static(__dirname + '/app'));
 
-app.listen(8888,function() {
+app.listen(port,function() {
   console.log("Gulp is running my app on port 8888");
 });
